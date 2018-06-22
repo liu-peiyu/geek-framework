02889b3fe2b9105a271c2816ea02f9cd4c2e76c1
放牛极客 <l_iupeiyu@qq.com>
2018-06-22 16:44:18 +0800

    修复服务器部署JVM不足的BUG

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f tpid
-nohup java -jar target/geek-framework.jar --spring.profiles.active=pro > /var/log/spring-boot-web/startup`date +%Y-%m-%d`.log 2>&1 &
+nohup java -Xmx512m -jar target/geek-framework.jar --spring.profiles.active=pro > /var/log/spring-boot-web/startup`date +%Y-%m-%d`.log 2>&1 &
 echo $! > tpid
 echo Start Success!
